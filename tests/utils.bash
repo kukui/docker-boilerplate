522066c09543fef377918619227363e86dc9f51c
kai keliikuli <kai.keliikuli@gmail.com>
2018-04-06 21:10:04 -0700

    utils.bash

--- tests/utils.bash
+++ tests/utils.bash
@@ -2,7 +2,7 @@
 
 docker_service_status () {
 export $(cat environments/$ENVIRONMENT | grep -v ^# | xargs)
-container_id=$(docker-compose -f docker-compose-local.yml ps -q $service)
+container_id=$(docker-compose  ps -q $service)
 inspect_json=$(docker inspect $container_id)
 #echo $inspect_json | jq -r '{running: .[0].State.Running, ports: .[0].NetworkSettings.Ports | keys| join(",")|test("3031/tcp")}| .running and .ports'
 status=$(echo "$inspect_json" | jq -r '{running: .[1].State.Running, ports: .[0].NetworkSettings.Ports | keys| join(",")|test("3031/tcp")}| .running and .ports')
