9d30b11ddad752fb27284e34c1d400d9a5ba897c
kai keliikuli <kai.keliikuli@gmail.com>
2018-04-07 06:20:30 -0700

    travis bats

--- tests/utils.bash
+++ tests/utils.bash
@@ -4,7 +4,6 @@ docker_service_status () {
 export $(cat environments/$ENVIRONMENT | grep -v ^# | xargs)
 container_id=$(docker-compose  ps -q $service)
 inspect_json=$(docker inspect $container_id)
-#echo $inspect_json | jq -r '{running: .[0].State.Running, ports: .[0].NetworkSettings.Ports | keys| join(",")|test("3031/tcp")}| .running and .ports'
 status=$(echo "$inspect_json" | jq -r '{running: .[1].State.Running, ports: .[0].NetworkSettings.Ports | keys| join(",")|test("3031/tcp")}| .running and .ports')
 return 0
 if [ $status == "true" ]; then
